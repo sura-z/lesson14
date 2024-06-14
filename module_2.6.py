a9f3faae60731924d810e0ac66c75154880ae982
sura-z <169043346+sura-z@users.noreply.github.com>
2024-06-14 12:13:32 +0300

    Add files via upload

---
+++ module_2.6.py
@@ -0,0 +1,18 @@
+def single_root_words(root_word, *other_words):
+    root_word = ['rich']
+    other_words = ['richiest', 'orichalcum', 'cheers', 'richies']
+    same_words = []
+    for i in root_woord:
+        if i in other_word:
+            same_words.append(i)
+    return same_words
+
+
+
+
+
+
+
+
+
+
